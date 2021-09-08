116ec079e7002c48a42940f47b743db5c2742b1d
Selvin Lisandro <selvin.lisandro@gmail.com>
2021-09-07 20:41:36 -0600

    update message modules and add docker files

--- Server/Procesos/readproc.c
+++ Server/Procesos/readproc.c
@@ -1,30 +1,35 @@
-#include <linux/module.h> 
-#include <linux/kernel.h>
 #include <linux/init.h>
-#include <linux/list.h>
-#include <linux/types.h>
-#include <linux/slab.h>
-#include <linux/sched.h>
-#include <linux/string.h>
-#include <linux/fs.h>
+#include <linux/module.h>
+#include <linux/kernel.h>
+#include <linux/debugfs.h>
 #include <linux/seq_file.h>
+#include <linux/spinlock.h>
+#include <linux/sched/signal.h>
+#include <linux/sched/task.h>
+
+/*para el archivo proc*/
+#include <linux/fs.h>
 #include <linux/proc_fs.h>
-#include <asm/uaccess.h> 
-#include <linux/hugetlb.h>
+#include <linux/seq_file.h>
+/*fin para el archivo proc*/
+
+
+#include <asm/processor.h>
+#include <asm/mmu_context.h>
 
 MODULE_LICENSE("GPL");
 MODULE_AUTHOR("Selvin Lisandro Aragón Pérez");
 MODULE_DESCRIPTION("Modulo para obtener la ram");
 MODULE_VERSION("0.01");
 
+#define FileProc "cpu_201325560"
+struct task_struct *task;
+
 static int my_proc_show(struct seq_file *m, void *v){
-    //Memoria en MB
-    struct sysinfo i;
-    si_meminfo(&i);
-    int32_t ramLibre = ((i.freeram*i.mem_unit)/(1024*1024));
-    int32_t ramTotal = ((i.totalram*i.mem_unit)/(1024*1024));
-    int32_t ramocupado = ramTotal - ramLibre;
-    seq_printf(m, "{ \"total\": %d , \"ocupado\": %d}", ramTotal, ramocupado);
+    //Procesos
+    for_each_process(task){
+        seq_printf(m, "{ \"id\": %d , \"name\": \"%s\" , \"status\": \"%ld\", \"fatherid\":\"0\"}",  task->pid, task->comm, task->state);
+  	}
     return 0;
 }
 
@@ -47,20 +52,19 @@ static struct file_operations my_fops = {
 
 static int __init init_p(void){
         struct proc_dir_entry *entry;
-        entry = proc_create("mem_grupo26-module", 0777, NULL, &my_fops);
+        entry = proc_create("proc_grupo26-module", 0777, NULL, &my_fops);
         if(!entry) {
                 return -1;
         } else {
-                printk(KERN_INFO "Hola mundo, somos el grupo 26 y este es el monitor de memoria \n");
+                printk(KERN_INFO "Hola mundo, somos el grupo 26 y este es el monitor de procesos \n");
         }
         return 0;
 }
 
 static void __exit exit_p(void){
-        remove_proc_entry("mem_grupo26-module",NULL);
-        printk(KERN_INFO "Sayonara mundo, somos el grupo 26 y este fue el monitor de memoria \n");
+        remove_proc_entry("proc_grupo26-module",NULL);
+        printk(KERN_INFO "Sayonara mundo, somos el grupo 26 y este fue el monitor de procesos \n");
 }
 
 module_init(init_p);
 module_exit(exit_p);
-

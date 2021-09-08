88e829835d90adc525e5f7bc743b9dbcdd562a00
Manases <2791808530307@ingenieria.usac.edu.gt>
2021-09-07 20:50:33 -0600

    Peticiones de servidor.

--- web-app/src/app/components/memory/dashboard.jsx
+++ web-app/src/app/components/memory/dashboard.jsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import clsx from 'clsx';
 import { makeStyles } from '@material-ui/core/styles';
 import CssBaseline from '@material-ui/core/CssBaseline';
@@ -63,8 +63,11 @@ const useStyles = makeStyles((theme) => ({
 
 export default function Dashboard() {
   const classes = useStyles();
+  const [data, setData] = useState([]);
   
   const fixedHeightPaper = clsx(classes.paper, classes.fixedHeight);
+
+  
 
   return (
     <div className={classes.root}>
--- web-app/src/app/services/server.jsx
+++ web-app/src/app/services/server.jsx
@@ -4,19 +4,83 @@ const API_URI = 'http://:5000';
 
 /**
  * {
- * 
+ *      total: numeric,
+ *      consumida: numeric,
+ *      porcentaje: numeric,
  * }
- * @returns 
  */
-
 export function memoryData(){
+    let max = 100;
+    let min = 10;
+    data = {
+        total: Math.random() * (max - min) + min,
+        consumida: Math.random() * (max - min) + min,
+        porcentaje: Math.random() * (max - min) + min,
+    }
+    
     return axios.get(`${API_URI}/memory`);
 }
 
+/**
+ * {
+ *      registered: numeric, //total de procesos registrados
+ *      running: numeric, //total de procesos en ejecución
+ *      sleeping: numeric, //total de procesos en ejecución
+ *      stopped: numeric, //total de procesos en ejecución
+ *      zombie: numeric, //total de procesos en ejecución
+ *      data: [
+ *          PID: numeric,
+ *          name: string,
+ *          estate: string,
+ *          ram: numeric,
+ *          taskCodesize: numeric,
+ *          user: string 
+ *      ]
+ * }
+ */
 export function processData(){
     return axios.get(`${API_URI}/process`);
 }
 
+/**
+ * data = {
+  id: 'root',
+  name: 'Parent',
+  children: [
+    {
+      id: '1',
+      name: 'Child - 1',
+    },
+    {
+      id: '3',
+      name: 'Child - 3',
+      children: [
+        {
+          id: '4',
+          name: 'Child - 4',
+        },
+      ],
+    },
+  ],
+}
+
+
+data = [
+    id: '2',
+    name: '',
+    father: id,
+]
+ * 
+ */
 export function treeData(){
     return axios.get(`${API_URI}/tree`);
+}
+
+/**
+ * Función para eliminar un proceso
+ * @param {*} pid del proceso al que se eliminara de la lista
+ * @returns {} 
+ */
+export function killProcess(pid){
+    return axios.post(`${API_URI}/killprocess`, {pid: pid});
 }

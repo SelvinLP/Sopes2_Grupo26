e3903c099de2af3e71425cdf859d3b2fb577a33d
Selvin Lisandro <selvin.lisandro@gmail.com>
2021-10-14 21:53:15 -0600

    Cambios eliminados por error al cambiar de carpeta

--- Server/Procesos/readproc.c
+++ Server/Procesos/readproc.c
@@ -43,17 +43,39 @@ static int my_proc_show(struct seq_file *m, void *v){
         }else if(task -> state == EXIT_ZOMBIE || task -> state ==  EXIT_DEAD){
             zombie++;
         }else{
-            registered--;
+            sleeping++;
         }
   	}
 
-    seq_printf(m, "{ \"registered\": %d , \"running\": %d , \"sleeping\": %d , \"stopped\": %d, \"zombie\": %d, data: [", registered, running, sleeping, stopped, zombie);
-
+    seq_printf(m, "{ \"registered\": %d , \"running\": %d , \"sleeping\": %d , \"stopped\": %d, \"zombie\": %d, \n \"data\": [", registered, running, sleeping, stopped, zombie);
+    seq_printf(m, "{\"father\":null, ");
+    seq_printf(m, "\"id\": 0, \"name\":\"Procesos\", \"estate\":\"Ejecutando\", \"ram\": 0, \"taskCodesize\":0, \"user\": \"0\" },\n");
     for_each_process(task){
-        get_task_struct(task);
+        get_task_struct(task);    
         if (task->mm) {
-            rss = get_mm_rss(task->mm) << PAGE_SHIFT;
-            seq_printf(m, "{\"PID\": %d, \"name\":\"%s\", \"estate\":%lu, \"ram\": %lu, \"taskCodesize\":%lu, \"user\":  \"%d\"},\n",task->pid, task->comm, task->state, rss, task->mm->end_code - task->mm->start_code, task->cred->uid.val);
+            seq_printf(m, "{\"father\":%d, ", task->parent->pid);
+            if(task -> state == TASK_RUNNING || task -> state == TASK_TRACED ){
+                rss = get_mm_rss(task->mm) << PAGE_SHIFT;
+                seq_printf(m, "\"id\": %d, \"name\":\"%s\", \"estate\":\"%s\", \"ram\": %lu, \"taskCodesize\":%lu",task->pid, task->comm, "Running", rss, task->mm->end_code - task->mm->start_code);
+            }else if(task -> state == TASK_INTERRUPTIBLE || task -> state == TASK_UNINTERRUPTIBLE ){
+                rss = get_mm_rss(task->mm) << PAGE_SHIFT;
+                seq_printf(m, "\"id\": %d, \"name\":\"%s\", \"estate\":\"%s\", \"ram\": %lu, \"taskCodesize\":%lu",task->pid, task->comm, "Sleeping", rss, task->mm->end_code - task->mm->start_code);
+            }else if(task -> state ==  __TASK_STOPPED || task -> state ==  TASK_STOPPED ){
+                rss = get_mm_rss(task->mm) << PAGE_SHIFT;
+                seq_printf(m, "\"id\": %d, \"name\":\"%s\", \"estate\":\"%s\", \"ram\": %lu, \"taskCodesize\":%lu",task->pid, task->comm, "Stoped", rss, task->mm->end_code - task->mm->start_code);
+            }else if(task -> state == EXIT_ZOMBIE || task -> state ==  EXIT_DEAD){
+                rss = get_mm_rss(task->mm) << PAGE_SHIFT;
+                seq_printf(m, "\"id\": %d, \"name\":\"%s\", \"estate\":\"%s\", \"ram\": %lu, \"taskCodesize\":%lu",task->pid, task->comm, "Zombie", rss, task->mm->end_code - task->mm->start_code);
+            }
+           
+        }else{
+            seq_printf(m, "{\"father\":%d, ", task->parent->pid);
+            seq_printf(m, "\"id\": %d, \"name\":\"%s\", \"estate\":\"%s\", \"ram\": 0, \"taskCodesize\":0",task->pid, task->comm, "sleeping");
+        }
+        if( task->cred->uid.val == 0){
+            seq_printf(m,", \"user\":  \"root\"},\n");
+        }else{
+            seq_printf(m,", \"user\":  \"%d\"},\n", task->cred->uid.val );
         }
     }
     seq_printf(m, "] \n }");
--- Server/server.go
+++ Server/server.go
@@ -8,7 +8,13 @@ import (
 	"log"
 	"net/http" // El paquete HTTP
 	"os"
+	"strings"
+	"syscall"
 )
+
+type pids struct {
+	Pid int
+}
 
 // Servidor HTTP
 func http_server(w http.ResponseWriter, r *http.Request) {
@@ -17,20 +23,15 @@ func http_server(w http.ResponseWriter, r *http.Request) {
 		http.Error(w, "404 not found.", http.StatusNotFound)
 		return
 	}
+	//Agregar header
+	w.Header().Set("Content-Type", "application/json")
+	w.Header().Set("Access-Control-Allow-Origin", "*")
+	w.Header().Set("Access-Control-Allow-Headers", "Content-Type")
 
 	switch r.Method {
 
 	case "GET":
-		//Agregar header
-		w.Header().Set("Content-Type", "application/json")
 
-		//Parsing body
-		var body map[string]interface{}
-		err := json.NewDecoder(r.Body).Decode(&body)
-		if err != nil {
-			fmt.Println("error")
-			return
-		}
 		//Tipos de peticiones
 		if r.URL.Path == "/memory" {
 			dat, errfile := os.ReadFile("/proc/mem_grupo26")
@@ -40,15 +41,52 @@ func http_server(w http.ResponseWriter, r *http.Request) {
 			w.WriteHeader(http.StatusCreated)
 			w.Write([]byte(jsonmem))
 			return
-		} else if r.URL.Path == "/process" {
+		} else if r.URL.Path == "/process" || r.URL.Path == "/tree" {
 			dat, errfile := os.ReadFile("/proc/procs_grupo26")
 			fmt.Print(errfile)
 			jsonmem := string(dat)
+
+			val := strings.Split(jsonmem, ",\n]")[0]
+			result := string(val) + "]\n}"
+
 			w.WriteHeader(http.StatusCreated)
-			w.Write([]byte(jsonmem))
+			w.Write([]byte(result))
 			return
 		}
+		return
+	case "POST":
 
+		//Parsing body
+		var body map[string]interface{}
+		err := json.NewDecoder(r.Body).Decode(&body)
+		if err != nil {
+			fmt.Println("error al parsear el body")
+			return
+		}
+		data, err := json.Marshal(body)
+		newData := string(data)
+		var pids_r pids
+		json.Unmarshal([]byte(newData), &pids_r)
+		//pid_result := strconv.Itoa(pids_r.Pid)
+
+		if r.URL.Path == "/killprocess" {
+			err := syscall.Kill(pids_r.Pid, syscall.SIGKILL)
+			//err := exec.Command("kill ", "-9 ", pid_result).Process
+
+			if err != nil {
+				jsonmem := string("{\"res\" : \"Operation not permitted\"}")
+				w.WriteHeader(http.StatusCreated)
+				w.Write([]byte(jsonmem))
+				fmt.Println(err)
+			} else {
+				jsonmem := string("{\"res\" : \"Ha sido detenido Correctamente\"}")
+				w.WriteHeader(http.StatusCreated)
+				w.Write([]byte(jsonmem))
+			}
+
+			return
+		}
+		return
 	default:
 		fmt.Fprintf(w, "Metodo %s no soportado \n", r.Method)
 		return
